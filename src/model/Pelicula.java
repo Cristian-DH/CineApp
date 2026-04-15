0d2287eef5465c09b7a94405ba1e4a2bcf752c88
Cristian Díaz <cristian.alexis.d.h@gmail.com>
2026-04-14 20:25:15 -0400

    featClasePelicula

--- src/model/Pelicula.java
+++ src/model/Pelicula.java
@@ -1,4 +1,24 @@
 package model;
 
-public class Pelicula {
+public class Pelicula { // Clase Pelicula
+
+    private String titulo; // Atributo título
+    private int duracion; // Duración en minutos
+
+    public Pelicula(String titulo, int duracion) { // Constructor
+        this.titulo = titulo; // Asigna título
+        this.duracion = duracion; // Asigna duración
+    }
+
+    public String getTitulo() { // Getter título
+        return titulo; // Retorna título
+    }
+
+    public int getDuracion() { // Getter duración
+        return duracion; // Retorna duración
+    }
+
+    public void mostrarInfo() { // Método para mostrar info
+        System.out.println("Película: " + titulo + " - " + duracion + " min"); // Imprime info
+    }
 }
